b78da3fa4d810e0bfe11994cb4af5af744bdffe2
AmrAbouelelaUWV <Amr.abouelela@uwv.nl>
2026-08-13 12:46:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,8 @@ resource "azurerm_private_dns_zone" "this" {
   name                = var.name
   resource_group_name = var.resource_group_name
 }
-
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
-  name                  = var.vnet_link_name
-  resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.this.name
-  virtual_network_id    = var.virtual_network_id
+  name                 = var.vnet_link_name
+  private_dns_zone_id  = azurerm_private_dns_zone.this.id
+  virtual_network_id   = var.virtual_network_id
 }
