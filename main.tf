8c1541da08a13c2cc0371e1592e257c891772416
amr.abouelela@uwv.nl <amr.abouelela@uwv.nl>
2026-08-17 11:57:49 +0200

    Add terraform ignore rules

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 resource "azurerm_private_dns_zone" "this" {
   name                = var.name
   resource_group_name = var.resource_group_name
+  tags                = local.tags
 }
+
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   name                 = var.vnet_link_name
   private_dns_zone_id  = azurerm_private_dns_zone.this.id
   virtual_network_id   = var.virtual_network_id
+  registration_enabled = var.registration_enabled
+  tags                 = local.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,42 @@
 variable "name" {
-  description = "Naam van de private DNS zone, bv. privatelink.blob.core.windows.net"
+  description = "Private DNS zone name, e.g. privatelink.blob.core.windows.net."
   type        = string
 }
 
 variable "resource_group_name" {
-  type = string
+  description = "Resource group where the private DNS zone is created."
+  type        = string
 }
 
 variable "virtual_network_id" {
-  description = "ID van het vnet dat gelinkt wordt aan deze DNS zone"
+  description = "Virtual network ID linked to the DNS zone."
   type        = string
 }
 
 variable "vnet_link_name" {
-  type    = string
-  default = "vnet-link"
+  description = "Name of the virtual network link."
+  type        = string
+  default     = "vnet-link"
+}
+
+variable "registration_enabled" {
+  description = "Whether auto-registration is enabled for the virtual network link."
+  type        = bool
+  default     = false
+}
+
+variable "biv" {
+  description = "Security classification tag, beschikbaarheid-integriteit-vertrouwelijkheid, 1=high 2=medium 3=low."
+  type        = string
+
+  validation {
+    condition     = can(regex("^[1-3]{3}$", var.biv))
+    error_message = "BIV must be three digits between 1 and 3, for example 122."
+  }
+}
+
+variable "tags" {
+  description = "Additional tags. The module-owned biv tag takes precedence."
+  type        = map(string)
+  default     = {}
 }
